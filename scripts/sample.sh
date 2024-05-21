19c9250385f28eec6cae1512f8364d44572c924e
gokuldevops20 <gokuldevops20@outlook.com>
2024-05-21 00:08:53 -0600

    Update sample.sh

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -7,7 +7,25 @@ gh extension upgrade github/gh-ado2gh
 
 # === Migration started ===
 
-gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest123
+echo "Repo migration started gokultest12301"
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest12301
+echo "Repo migration end gokultest12301"
+
+echo "Repo migration started gokultest12302"
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest12302
+echo "Repo migration end gokultest12302"
+
+echo "Repo migration started gokultest12303"
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest12303
+echo "Repo migration end gokultest12303"
+
+echo "Repo migration started gokultest12304"
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest12304
+echo "Repo migration end gokultest12304"
+
+echo "Repo migration started gokultest12305"
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest12305
+echo "Repo migration end gokultest12305"
 
 # === Migration end ===
 
