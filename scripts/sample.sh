8ef6212b8fd0f08caca9e6569af2b64540feba4f
gokuldevops20 <gokuldevops20@outlook.com>
2024-05-20 22:44:10 -0600

    Update sample.sh

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,7 +1,7 @@
 echo "Install github migration tool"
 
-export GH_TOKEN="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"   
-export GH_PAT="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"
+export GH_TOKEN="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"   
+export GH_PAT="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"
 export ADO_PAT="oyn3t63awagzl7qyqps3e2l4btimf45rfx2hwuvmsoo3z5tnmfuq"
 
 gh extension install github/gh-ado2gh
