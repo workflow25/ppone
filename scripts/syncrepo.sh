d0d92d57401caa25c3cb362f20b9d98a5569c1e2
gokuldevops20 <gokuldevops20@outlook.com>
2024-05-21 01:54:18 -0600

    Update syncrepo.sh

--- scripts/syncrepo.sh
+++ scripts/syncrepo.sh
@@ -4,7 +4,7 @@ pwd
 mkdir sourcerepo
 cd sourcerepo
 pwd
-echo "ado pat ${{vars.ADO_PAT}}"
-git clone https://${{vars.ADO_PAT}}@dev.azure.com/gokuldevops20/gokultest/_git/gokultest
+echo "ado pat ${{ vars.ADO_PAT }}"
+git clone https://${{ vars.ADO_PAT }}@dev.azure.com/gokuldevops20/gokultest/_git/gokultest
 cd gokultest
 ls
