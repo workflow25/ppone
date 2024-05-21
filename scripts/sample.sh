7153a27828adc0363c2c3e935708b9158b4c5bf6
gokuldevops20 <gokuldevops20@outlook.com>
2024-05-20 22:33:27 -0600

    Update sample.sh

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,2 +1,12 @@
 echo "Install github migration tool"
-gh extension install github/gh-ado2gh
+
+export GH_PAT="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"
+export ADO_PAT="oyn3t63awagzl7qyqps3e2l4btimf45rfx2hwuvmsoo3z5tnmfuq"
+
+# === Migration started ===
+
+gh ado2gh migrate-repo --ado-org gokuldevops20 --ado-team-project gokultest --ado-repo gokultest --github-org  workflow25 --github-repo gokultest123
+
+# === Migration end ===
+
+
