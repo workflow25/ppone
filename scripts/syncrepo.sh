f7142e0fe10119eb95e39220468a51d79aa425af
gokuldevops20 <gokuldevops20@outlook.com>
2024-05-21 02:54:07 -0600

    Update syncrepo.sh

--- scripts/syncrepo.sh
+++ scripts/syncrepo.sh
@@ -18,36 +18,4 @@ pwd
 ls
 git checkout dev
 ls
-
-echo "##################Clone destination repo##########################"
-
-cd /home/runner/work/ppone/ppone/reposync
-ls
-
-echo "################## make destination repo##########################"
-mkdir destinationrepo
-cd destinationrepo
-pwd
-ls
-
-git clone https://$GH_TOKEN@github.com/workflow25/dummy12301.git
-cd dummy12301
-echo "##################destination repo files ##########################"
-pwd
-ls
-git checkout dev
-ls
-
-echo "##################Move to root folder ##########################"
-cd /home/runner/work/ppone/ppone/reposync
-pwd
-ls
-
-cp -r sourcerepo/gokultest/ destinationrepo/dummy12301
-
-cd /home/runner/work/ppone/ppone/reposync/destinationrepo/dummy12301
-ls
-
-
-
-
+git push --prune h https://$GH_TOKEN@github.com/workflow25/dummy12301.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
